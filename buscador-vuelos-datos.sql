c92e2aa31e2dda0ec1ebdb0f0936289305882e79
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-02-26 09:33:14 +0100

    teeu

--- buscador-vuelos-datos.sql
+++ buscador-vuelos-datos.sql
@@ -1,7 +1,7 @@
 INSERT INTO AEROPUERTO VALUES('MAD','Barajas-Adolfo Suarez','Madrid','España'),('LHR','Heathrow','Londres','Inglaterra'),('CDG','Paris-Charles de Gualle','Paris','Francia'),('MEX','Benito Juarez','Ciudad de Mexico','Mexico'),('BCN','El Prat','El Prat de Llobregat','España');
 INSERT INTO TERMINAL VALUES('T4','MAD','Madrid'),('North','LHR','Londres'),('2C','CDG','Paris'),('T3','MEX','Ciudad de Mexico'),('T2B','BCN','El Prat de Llobregat');
 INSERT INTO COMPAÑIA VALUES('RYR','Ryanair',lo_import'ryanair.png'),(),(),(),();
-INSERT INTO VUELO VALUES(),(),(),(),();
+INSERT INTO VUELO VALUES('IB-2367','IBE','Barajas-Adolfo Suarez','Heathrow','Situado','4','1'),('FR-2377','RYR','Paris-Charles de Gualle','Heathrow','Cancelado','3','2'),('UX-2567','AEA','Barajas-Adolfo Suarez','Benito Juarez','Retrasado','5','8'),('EK-2467','UAE','El Prat','Heathrow','Retrasado','1','4'),('EK-2467','UAE','El Prat','Barajas-Adolfo Suarez','Cancelado','3','5');
 /*INSERT INTO ASIENTO VALUES(),(),(),(),();
 INSERT INTO PASAJERO VALUES(),(),(),(),();
 INSERT INTO RESERVA VALUES(),(),(),(),();
