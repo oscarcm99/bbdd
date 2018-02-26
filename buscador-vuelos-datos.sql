e57eee30b3d55cd3faaf433a81728372b5814db8
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-02-26 09:10:18 +0100

    tet

--- buscador-vuelos-datos.sql
+++ buscador-vuelos-datos.sql
@@ -1,5 +1,5 @@
 INSERT INTO AEROPUERTO VALUES('MAD','Barajas-Adolfo Suarez','Madrid','España'),('LHR','Heathrow','Londres','Inglaterra'),('CDG','Paris-Charles de Gualle','Paris','Francia'),('MEX','Benito Juarez','Ciudad de Mexico','Mexico'),('BCN','El Prat','El Prat de Llobregat','España');
-INSERT INTO TERMINAL VALUES(2),(),(),(),();
+INSERT INTO TERMINAL VALUES('T4','MAD','Madrid'),('North','LHR','Londres'),('2C','CDG','Paris'),('T3','MEX','Ciudad de Mexico'),('T2B','BCN','El Prat de Llobregat');
 INSERT INTO VUELO VALUES(),(),(),(),();
 INSERT INTO ASIENTO VALUES(),(),(),(),();
 INSERT INTO PASAJERO VALUES(),(),(),(),();
